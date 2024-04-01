f5cf8a9b721cbd9079abdb9e62e2c34d4594b382
marcelldac <devmarcell@protonmail.com>
2024-03-31 22:39:47 -0300

    feat: user interface

---
+++ src/interfaces/user-interface.ts
@@ -0,0 +1,22 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface UserCreate {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface IUserRepository {
+  create(data: UserCreate): Promise<User>;
+  findByEmail(email: string): Promise<User | null>;
+  findByID(id: string): Promise<User | null>;
+  update(data: UserCreate, id: string): Promise<User>;
+  remove(id: string): Promise<void>;
+}
