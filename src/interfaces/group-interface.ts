0d880eba4b44cec71494abe1a275bce6a19e9928
marcelldac <devmarcell@protonmail.com>
2024-04-01 07:45:17 -0300

    feat: group interface

---
+++ src/interfaces/group-interface.ts
@@ -0,0 +1,25 @@
+export interface Group {
+  id: string;
+  name: string;
+  description: string | null;
+  creator_id: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface CreateGroup {
+  name: string;
+  description: string | null;
+  creator_id: string;
+}
+
+export interface IGroupRepository {
+  create(data: CreateGroup): Promise<Group>;
+  findAll(): Promise<Group[] | []>;
+  findByID(id: string): Promise<Group | null>;
+  findByUser(creator_id: string): Promise<Group[] | null>;
+  update(data: CreateGroup, id: string): Promise<Group>;
+  remove(id: string): Promise<void>;
+  addUserInGroup(user_id: string, group_id: string): Promise<Group>;
+  removeUserFromGroup(user_id: string, group_id: string): Promise<Group>;
+}
