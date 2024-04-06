703e10a46b4d9675d99b96bd734049bb79312598
marcelldac <devmarcell@protonmail.com>
2024-04-06 13:50:18 -0300

    feat: group routes

---
+++ src/routes/group-routes.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance } from "fastify";
+import { GroupUseCase } from "../use-cases/group-use-case";
+import { CreateGroup } from "../interfaces/group-interface";
+
+export async function groupRoutes(fastify: FastifyInstance) {
+  const groupUseCase = new GroupUseCase();
+
+  fastify.get("/", async (_, reply) => {
+    try {
+      const data = await groupUseCase.findAll();
+      reply.send(data);
+    } catch (err) {
+      reply.send(err);
+    }
+  });
+
+  fastify.get<{ Params: { id: string } }>("/:id", async (req, reply) => {
+    try {
+      const data = await groupUseCase.findByID(req.params.id);
+      reply.send(data);
+    } catch (err) {
+      reply.send(err);
+    }
+  });
+
+  fastify.post<{ Body: CreateGroup }>("/", async (req, reply) => {
+    try {
+      const data = await groupUseCase.create(req.body);
+      reply.send(data);
+    } catch (err) {
+      reply.send(err);
+    }
+  });
+
+  fastify.put<{ Body: CreateGroup; Params: { id: string } }>(
+    "/:id",
+    async (req, reply) => {
+      try {
+        const data = await groupUseCase.update(req.body, req.params.id);
+        reply.send(data);
+      } catch (err) {
+        reply.send(err);
+      }
+    }
+  );
+
+  fastify.delete<{ Params: { id: string } }>("/:id", async (req, reply) => {
+    try {
+      await groupUseCase.remove(req.params.id);
+      reply.send({ message: "User deleted." });
+    } catch (err) {
+      reply.send(err);
+    }
+  });
+}
