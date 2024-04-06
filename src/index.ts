a9b9fd81359377f718d125a795eb22e578b4ccba
marcelldac <devmarcell@protonmail.com>
2024-04-06 13:50:30 -0300

    feat: register group routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
 import { userRoutes } from "./routes/user-routes";
+import { groupRoutes } from "./routes/group-routes";
 
 const port = Number(process.env.PORT) || 3333;
 const app = fastify();
 
 app.register(userRoutes, { prefix: "/api/v1/user" });
+app.register(groupRoutes, { prefix: "/api/v1/group" });
 
 app.listen({ port }, () => {
   console.log(`App running on ${port}`);
